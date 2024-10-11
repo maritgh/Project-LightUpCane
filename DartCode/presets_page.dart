bf086dfe23c241a9d292f21bbdd5120d68e8a686
DanielHuijsdens <166701770+DanielHuijsdens@users.noreply.github.com>
2024-10-11 23:12:52 +0200

    Renamed profile to presets

--- DartCode/presets_page.dart
+++ DartCode/presets_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget{  
+class PresetsPage extends StatelessWidget{  
 
   @override
   Widget build(BuildContext context) {
