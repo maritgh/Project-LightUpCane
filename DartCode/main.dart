050f4e8259f56d7e85a605ad0be3954755f113dd
Rae <98871609+Rszh3ng@users.noreply.github.com>
2024-10-31 16:53:58 +0100

    Update main.dart
    added change notifier for theme
    *not fully functional yet, will need adjustment

--- DartCode/main.dart
+++ DartCode/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import 'my_app.dart';
+//added
+import 'package:provider/provider.dart';
+import 'theme_provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: MyApp(),
+    ),
+  );
 }
+
+// original
+//void main() {
+//  runApp(MyApp());
+//}
