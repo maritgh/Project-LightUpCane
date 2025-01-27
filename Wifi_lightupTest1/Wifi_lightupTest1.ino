8f70cec703f66e1664b4b9ad459a5bee5bcbb5bc
Israa <127145513+israabens@users.noreply.github.com>
2025-01-27 11:33:18 +0100

    Add files via upload

---
+++ Wifi_lightupTest1/Wifi_lightupTest1.ino
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <MPU6050.h>
+#include "variables.h"
+#include <WiFi.h>
+#include "user_settings.h"
+#include <WebServer.h>
+#include <Preferences.h>
+
+Preferences preferences;
+
+WebServer server(80);
+
+void setup() {
+  init_hardware();
+  init_imu();
+  loadProfileSettings();
+  Serial.println("\nESP32 Initialized"); // print al the start values
+
+#ifdef Debug
+  Serial.print("Intensity haptic: ");
+  Serial.println(profiles.intensity_haptic);
+  Serial.print("Intensity buzzer: ");
+  Serial.println(profiles.intensity_buzzer);
+  Serial.print("Intensity led: " );
+  Serial.println(profiles.intensity_led);
+#endif
+
+  Serial.println("\n\nConfiguring access point..."); //starting the hotspot 
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  
+
+  server.on("/set", HTTP_POST, []() { // to put the data send in the right spot
+    if (server.hasArg("data")) {
+      String data = server.arg("data");
+      splitStringBySpace(data);
+    } else {
+      server.send(400, "text/plain", "Invalid data");
+    }
+  });
+
+  server.on("/get", HTTP_GET, []() { // to send data to the app
+    float bat = (bat_status()/9)*100;
+    String data = String(bat) + " " + String(revers_calc_intensity(profiles.intensity_led)) + " " + String(revers_calc_intensity(profiles.intensity_buzzer)) + " " + String(profiles.frequency) + " " + String(revers_calc_intensity(profiles.intensity_haptic)) + " " + String(power);
+    server.send(200, "text/plain", data);
+  });
+  
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+
+void loop() {
+  server.handleClient();
+
+  currentState = digitalRead(SW_ON_BOOT);
+  if (lastState == HIGH && currentState == LOW) {
+    pressedTime = millis();
+    delay(100);
+  } else if (lastState == LOW && currentState == HIGH) {  // button is released
+    releasedTime = millis();
+    pressDuration = releasedTime - pressedTime;
+
+    if ((pressDuration > profiles.time_battery_status) && power ) {
+      check_battery_status = true;
+    } else if (pressDuration > 0) {
+      short_press = true;
+    }
+    delay(100);
+  }
+  lastState = currentState;
+  if (short_press) {
+    short_press = false;
+    toggle_power();
+  } else if (check_battery_status && power) {
+#ifdef Debug
+    Serial.println("Check battery status");
+    Serial.print("Battery is: ");
+    Serial.print(bat_voltage);
+    Serial.println("V");
+#endif
+    trig_feedback(battery_filter);
+  }
+}
---
+++ Wifi_lightupTest1/variables.h
@@ -0,0 +1,57 @@
+/* DEFINES PINS*/
+#define SCL_PIN 1
+#define SDA_PIN 0
+#define BUZZER 8
+#define HAPTIC 10
+#define LED_LIGHTS 2
+#define SW_ON_BOOT 9
+#define BATT_SENS 3
+
+/* DEBUG DEFINES */
+#define Debug
+
+// WiFi access point credentials
+const char* ssid = "light_up_cane";
+const char* password = "12345678";
+
+
+/* VARIABLES */
+bool power = false;           // the current state of LED
+bool imu = false;
+bool short_press = false;
+bool check_battery_status = false;
+
+float bat_voltage = 0.0; // ADC batt sens
+float bat_voltages[20];  // array for battery voltages
+
+int check = 0;
+int ctr = 0;
+int lastState = HIGH;  // the previous state from the input pin
+int currentState;     // the current reading from the input pin
+int battery_status = 4;
+
+int haptic_on_times = 1;  //how many times haptic when light is turned on
+int haptic_off_times = 2; //how many times haptic when light is turned off
+
+int buzzer_on_times = 1; //how many times buzzer when light is turned on
+int buzzer_off_times = 2;  //how many times buzzer when light is turned off
+
+int battery_filter = 1;
+int power_filter = 2;
+
+long pressDuration = 0;
+unsigned long pressedTime  = 0;
+unsigned long releasedTime = 0;
+
+hw_timer_t *imu_timer = NULL;
+hw_timer_t *imu_blink_timer = NULL;
+
+MPU6050 mpu;
+
+
+
+
+
+
+
+
