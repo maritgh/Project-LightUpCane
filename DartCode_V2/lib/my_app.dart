c7be30e502f90ccc45739e37167438b7bb0139b3
Rae <98871609+Rszh3ng@users.noreply.github.com>
2025-02-01 18:42:34 +0100

    Removed excess comments

--- DartCode_V2/lib/my_app.dart
+++ DartCode_V2/lib/my_app.dart
@@ -55,17 +55,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-
-// original
-// import 'package:flutter/material.dart';
-// import 'home_page.dart';
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: HomePage(),
-//     );
-//   }
-// }
